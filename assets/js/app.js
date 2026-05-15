1881aa47d8d646cff2c36b91bc9cd3536c168d33
Pioneercode <pioneercode@outlook.com>
2026-05-15 11:43:01 +0300

    Changes to be committed

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+
+
+
+const currentStep = {stat: 0};
+const userInfo = { name: '', email: '', phone: '', address: '', city: '', state: '', zip: '', country: '', cardNumber: '', cardName: '', cardExp: '', cardCVC: ''};
+
+function updateUI() {
+
+}
+
+function updateProgress() {
+
+}
+
+function validateStep(step) {
+    
+}
+
+updateUI();
+updateProgress();
