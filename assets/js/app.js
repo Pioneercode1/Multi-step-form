b25ae9bf001ba8d11bc27724ee5d6610ea6aa81f
Pioneercode <pioneercode@outlook.com>
2026-05-16 17:57:58 +0300

    add lines in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
-
-
-
+/* elements */
+const multiForm = document.getElementById("#multi-form");
+const stepIndex = document.querySelectorAll(".step-item-index");
+const stepCard = document.querySelectorAll(".step-card");
+/* buttons */
+const btnBack = document.querySelector(".btn-back");
+const btnNext = document.querySelector(".btn-next");
+const btnConfirm = document.querySelector(".btn-confirm");
+/**/
+const stepConfirmation = document.querySelector(".step-confirmation");
+/**/
 const currentStep = {stat: 0};
 const userInfo = { name: '', email: '', phone: '', address: '', city: '', state: '', zip: '', country: '', cardNumber: '', cardName: '', cardExp: '', cardCVC: ''};
 
