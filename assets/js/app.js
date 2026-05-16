78e9076c874107b1228b8835d2064c98ba2d688d
Pioneercode <pioneercode@outlook.com>
2026-05-16 20:54:20 +0300

    add functions for active steps

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 /* elements */
-const multiForm = document.getElementById("#multi-form");
-const stepIndex = document.querySelectorAll(".step-item-index");
+const multiForm = document.getElementById("multi-form");
+const stepNumber = document.querySelectorAll(".step-item-index");
 const stepCard = document.querySelectorAll(".step-card");
 /* buttons */
 const btnBack = document.querySelector(".btn-back");
@@ -9,20 +9,56 @@ const btnConfirm = document.querySelector(".btn-confirm");
 /**/
 const stepConfirmation = document.querySelector(".step-confirmation");
 /**/
-const currentStep = {stat: 0};
-const userInfo = { name: '', email: '', phone: '', address: '', city: '', state: '', zip: '', country: '', cardNumber: '', cardName: '', cardExp: '', cardCVC: ''};
+let currentStep = 0;
 
-function updateUI() {
-
-}
 
 function updateProgress() {
-
-}
-
-function validateStep(step) {
     
+    stepNumber.forEach((num, index) => {
+        
+        num.classList.toggle("step-active", index === currentStep);
+    });
 }
 
-updateUI();
+function updateUI() {
+    stepCard.forEach((step, index) => {
+        step.classList.toggle("active", index === currentStep);
+    });
+}
+
+multiForm.addEventListener("click", e => {
+    let inputs = stepCard[currentStep].querySelectorAll("input");
+    let valid = [...inputs].every(input => {
+        return input.checkValidity();
+    });
+    //
+    if (e.target.matches("[data-next]")) {
+        if (currentStep < stepCard.length - 1) {
+            if (!valid) {
+                inputs.forEach(input => {
+                    input.reportValidity();
+                });
+            }
+        }
+        currentStep++;
+        updateProgress();
+        updateUI();
+
+    } else if (e.target.matches("[data-back]")) {
+        if (currentStep > 0) {
+            currentStep--;
+            updateProgress();
+            updateUI();
+        }
+    }
+});
+
 updateProgress();
+updateUI();
+
+multiForm.addEventListener("submit", event => {
+    event.preventDefault();
+});
+
+
+const userInfo = { name: '', email: '', phone: '', address: '', city: '', state: '', zip: '', country: '', cardNumber: '', cardName: '', cardExp: '', cardCVC: '' };
