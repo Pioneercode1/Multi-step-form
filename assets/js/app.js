5600d9fce9f3829462b6e13454337c71e9032b19
Pioneercode <pioneercode@outlook.com>
2026-05-18 00:17:06 +0300

    some changes for dynamically add subscription and add-on selections

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,8 @@ const planPricePro = document.getElementById("plan-price-pro");
 const onLabelYearly = document.querySelector(".switch");
 const onYearly = document.getElementById("billing-cycle");
 const freeTwoMonths = document.querySelectorAll(".data-free");
+const monthlyActive = document.querySelector(".monthly");
+const yearlyActive = document.querySelector(".yearly");
 /* addon selected*/
 const addonService = document.getElementById("addon-price-service");
 const addonStorage = document.getElementById("addon-price-storage");
@@ -22,9 +24,95 @@ const onCustom = document.getElementById("addon-customizable-profile");
 const btnBack = document.querySelector(".btn-back");
 const btnNext = document.querySelector(".btn-next");
 const btnConfirm = document.querySelector(".btn-confirm");
-/**/
 const stepConfirmation = document.querySelector(".step-confirmation");
+/* summary */
+const planItemContainer = document.querySelectorAll("[plan-option]");
+const summaryTitleGroup = document.querySelector(".plan-title-group")
+//
+const optionContainer = document.querySelectorAll("[addon-option]");
+const summaryOptionGroup = document.querySelector(".summary-option");
+const totalPrice = document.querySelector(".total-price");
 /**/
+
+function createSummaryOptions(valueOptionTitle, valueOptionPrice) {
+    const summaryOptionTitle = document.createElement("span");
+    summaryOptionTitle.classList.add("summary-option-title");
+    const summaryOptionPrice = document.createElement("span");
+    summaryOptionPrice.classList.add("option-price");
+    //
+    newValueOptionTitle = document.createTextNode(valueOptionTitle);
+    newValueOptionPrice = document.createTextNode(valueOptionPrice);
+    //
+    summaryOptionTitle.appendChild(newValueOptionTitle);
+    summaryOptionPrice.appendChild(newValueOptionPrice);
+    //
+    summaryOptionTitle.appendChild(summaryOptionPrice);
+    summaryOptionGroup.innerHTML = "";
+    summaryOptionGroup.append(summaryOptionTitle);
+}
+
+function createSummaryPlan(valuePlanTitle, valuePlanPrice) {
+    const planTitle = document.createElement("span");
+    planTitle.classList.add("summary-plan-title");
+    const summaryPlanPrice = document.createElement("span");
+    summaryPlanPrice.classList.add("plan-price");
+    //
+    newValuePlanTitle = document.createTextNode(valuePlanTitle);
+    newValuePlanPrice = document.createTextNode(valuePlanPrice);
+    //
+    planTitle.appendChild(newValuePlanTitle);
+    summaryPlanPrice.appendChild(newValuePlanPrice);
+
+    //
+    planTitle.appendChild(summaryPlanPrice);
+    summaryTitleGroup.innerHTML = "";
+    summaryTitleGroup.append(planTitle);
+}
+
+/* create Summary Plan */
+for (let plan = 0; plan < planItemContainer.length; plan++) {
+    const element = planItemContainer[plan];
+        element.addEventListener("click", e => {
+            let valuePlanPrice ="";
+            if (!e.target.getAttribute("e.target")) {
+                for (const key of planItemContainer) {
+                    key.removeAttribute("checked");
+                }
+                e.target.setAttribute("checked", "");
+                const valuePlanTitle = e.target.value;
+                if (onYearly) {
+                    valuePlanPrice = e.target.getAttribute("data-yearly");
+                } else {
+                    valuePlanPrice = e.target.getAttribute("data-monthly");
+                }
+                createSummaryPlan(valuePlanTitle, valuePlanPrice);
+            }
+        });
+}
+
+/* create Summary Options */
+for (let plan = 0; plan < optionContainer.length; plan++) {
+    const element = optionContainer[plan];
+    element.addEventListener("click", e => {
+        let valueOptionPrice = "";
+        if (!e.target.getAttribute("e.target")) {
+            for (const key of optionContainer) {
+                key.removeAttribute("checked");
+            }
+            e.target.setAttribute("checked", "");
+            const valueOptionTitle = e.target.value;
+            //const valueOptionPrice = "";
+            if (onYearly) {
+                valueOptionPrice = e.target.getAttribute("data-yearly");
+            } else {
+                valueOptionPrice = e.target.getAttribute("data-monthly");
+            }
+            createSummaryOptions(valueOptionTitle, valueOptionPrice);
+        }
+    });
+}
+//
+
 let currentStep = 0;
 
 
@@ -70,14 +158,32 @@ multiForm.addEventListener("click", e => {
     }
 });
 
-onYearly.addEventListener("change", () => {
-    if(onYearly) {
+// select your plan
+onYearly.addEventListener("click", () => {
+    if (onYearly) {
         freeTwoMonths.forEach(span => {
             span.toggleAttribute("hidden");
         });
+
+        const validYear = onLabelYearly.toggleAttribute("data-valid");
+
+        if (!validYear) {
+            planPriceArcade.innerText = planPriceArcade.getAttribute("data-yearly");
+            planPriceAdvanced.innerText = planPriceAdvanced.getAttribute("data-yearly");
+            planPricePro.innerText = planPricePro.getAttribute("data-yearly");
+            monthlyActive.classList.toggle("active-time");
+            yearlyActive.classList.toggle("active-time");
+        } else if (validYear) {
+            planPriceArcade.innerText = planPriceArcade.getAttribute("data-monthly");
+            planPriceAdvanced.innerText = planPriceAdvanced.getAttribute("data-monthly");
+            planPricePro.innerText = planPricePro.getAttribute("data-monthly");
+            monthlyActive.classList.toggle("active-time");
+            yearlyActive.classList.toggle("active-time");
+        }
     }
 });
 
+// update form
 updateProgress();
 updateUI();
 
